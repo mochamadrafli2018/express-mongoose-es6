21ccc18790a402b0fe94471ff7820552e751a326
Mochamad Rafli Ramadhan <mochamadrafli2018@mail.ugm.ac.id>
2022-04-17 00:22:44 +0700

    Update mongodb.config.js

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.ATLAS_URI,
+mongoose.connect(process.env.MONGO_URI,
   function(err) {
     if (err) {
       console.log('Database connection is failed. exiting now...');
       console.log('Check internet connection stable or not');
-      console.log('Check mongodb url in .env and make sure it is not wrong');
+      console.log('Check mongodb URL in .env and make sure it is not wrong');
     }
     console.log('Successfully connected to mongodb database');
   }
